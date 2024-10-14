5a01e681b9f0a4e15e6d82f07ba7b54aac67f720
William Almeida <william.cesar.silva@hotmail.com>
2024-10-14 14:19:20 -0300

    instaladorcomflow

---
+++ README.md
@@ -0,0 +1,10 @@
+FAZENDO DOWNLOAD DO INSTALADOR & INICIANDO A PRIMEIRA INSTALAÇÃO (USAR SOMENTE PARA PRIMEIRA INSTALAÇÃO):
+
+```bash
+sudo apt install -y git && git clone https://github.com/Cristian2704a/Install_Whaticket_GBSOficial.git && sudo chmod -R 777 Install_Whaticket_GBSOficial && cd Install_Whaticket_GBSOficial && sudo ./instalador
+```
+
+ACESSANDO DIRETORIO DO INSTALADOR & INICIANDO INSTALAÇÕES ADICIONAIS (USAR ESTE COMANDO PARA SEGUNDA OU MAIS INSTALAÇÃO:
+```bash
+cd ./Install_Whaticket_GBSOficial sudo ./instalador_2
+```
---
+++ utils/_banner.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+
+print_banner() {
+
+  clear
+
+printf "\n\n"
+printf "${ORANGE}";  
+printf " ██████╗ ██████╗ ███████╗\n";
+printf "██╔════╝ ██╔══██╗██╔════╝\n";
+printf "██║  ███╗██████╔╝███████\n";
+printf "██║   ██║██╔══██╗╚════██\n";
+printf "╚██████╔╝██████╔╝███████║\n";
+printf " ╚═════╝ ╚═════╝ ╚══════╝\n";
+printf "${NC}";
+
+
+
+  printf "\n"
+}
